89018b4ab15ca8cb1f0807891e85eb1e1364f5b4
RealPolitiX <xrpatrick@gmail.com>
2018-06-24 05:23:38 +0200

    Added jit-version of calcStructureFactors in fed.nbcalcStructureFactors()

--- fedpy/fed.py
+++ fedpy/fed.py
@@ -4,6 +4,7 @@ from __future__ import division
 from . import utils as u
 import numpy as np
 import pandas as pd
+import numba as nb
 import itertools as it
 import pasim as ps
 pi = np.pi
@@ -45,12 +46,12 @@ class Detector(object):
                 self.centreX, self.centreY, self.dist, self.pixelSize, \
                 self.magnification))
 
-        
+
 class ElectronBeam(object):
-    
+
     # Electron beam parameters
     def __init__(self, xyzFractionals, voltage, Crystal):
-        
+
         self.dirFractionals = xyzFractionals
         dirCart = frac2cart(xyzFractionals,Crystal.axes)
         self.dirCart_norm = dirCart/np.linalg.norm(dirCart)
@@ -68,9 +69,9 @@ class ElectronBeam(object):
 
 
 class TriclinicCrystal(object):
-    
+
     def __init__(self, a, b, c, alpha, beta, gamma):
-        
+
         # Basic geometric parameters
         deg2rad = np.pi/180.0
         self.a = a
@@ -84,7 +85,7 @@ class TriclinicCrystal(object):
         gamma = gamma*deg2rad
         V = a*b*c*((1-np.cos(alpha)**2-np.cos(beta)**2-np.cos(gamma)**2 + \
                     2*np.cos(alpha)*np.cos(beta)*np.cos(gamma))**0.5)
-        
+
         # Transformation matrix and its inverse
         T = np.zeros([3,3])
         T[0,:] = [a, b*np.cos(gamma), c*np.cos(beta)]
@@ -94,21 +95,21 @@ class TriclinicCrystal(object):
         self.axes = np.transpose(T)
         M = np.linalg.inv(T)
         self.T_xyz2uvw = M
-        
+
         # Reciprocal space axis vectors
         A = T[:,0]
         B = T[:,1]
         C = T[:,2]
-        CP = np.cross(B,C)   
+        CP = np.cross(B,C)
         Astar = CP/np.dot(A,CP)
         CP = np.cross(C,A)
         Bstar = CP/np.dot(B,CP)
         CP = np.cross(A,B)
         Cstar= CP/np.dot(C,CP)
         self.axesRecip = np.array([Astar, Bstar, Cstar])
-        
+
     def __repr__(self):
-        
+
         return("Triclinic crystal\n\
                 a = {},\n\
                 b = {},\n\
@@ -125,7 +126,7 @@ def voltage2wavelength(voltage):
     """C onvert acceleration voltage of the electron
     to its de Broglie wavelength (in Angstrom)
     """
-    
+
     h = 6.626069E-34
     e = 1.602176E-19
     me = 9.10938E-31
@@ -133,42 +134,42 @@ def voltage2wavelength(voltage):
     term1 = h/np.sqrt(2*me*e*voltage)
     term2 = 1.0/np.sqrt(1 + (e*voltage/(2*me*c*c)))
     eWavelength = term1*term2*1e10 # unit in Angstrom
-    
+
     return eWavelength
 
 def frac2cart(xyzFrac,axes):
     """
     Conversion from fractional to Cartesian coordinates
     """
-    
+
     xyzCart = np.dot(xyzFrac,axes)
-    
+
     return xyzCart
 
 def hkl2cart(hkl,axesRecip):
-    
+
     xyzCart = np.dot(hkl,axesRecip)
-    
+
     return xyzCart
 
 def cart2frac(xyzCart,cellAxes):
     """
     Conversion from Cartesian to fractional coordinates
     """
-    
+
     invAxes = np.linalg.inv(cellAxes)
     xyzFrac = np.array(np.dot(xyzCart,invAxes),'float64')
-    
+
     return xyzFrac
 
 def atomLabels2numbers(labels):
     """
     Convert atomic symbols to atomic number
     """
-    
+
     nAt = labels.size
     atomicNumbers = np.zeros(nAt)
-    
+
     for atm in range(nAt):
         if labels[atm] == 'H':
             atomicNumbers[atm] = 1
@@ -180,7 +181,7 @@ def atomLabels2numbers(labels):
             atomicNumbers[atm] = 53
         else:
             print('atom {} not found'.format(atm))
-    
+
     return atomicNumbers
 
 
@@ -194,18 +195,18 @@ def calcAtomicF(Z,modQ):  # modQ = s/(2*pi)
     B = np.array([KirkTableMat[L0,1],KirkTableMat[L0,3],KirkTableMat[L0+1,1]]).reshape(3,1)
     C = np.array([KirkTableMat[L0+1,2],KirkTableMat[L0+2,0],KirkTableMat[L0+2,2]]).reshape(1,3)
     D = np.array([KirkTableMat[L0+1,3],KirkTableMat[L0+2,1],KirkTableMat[L0+2,3]]).reshape(3,1)
-    q2 = np.tile(modQ**2,(3,1))   
+    q2 = np.tile(modQ**2,(3,1))
     E = 1.0/(q2 + B)
     term1 = np.dot(A, E)
     term2 = np.dot(C, np.exp(-q2*D))
     atmF = term1 + term2
-    
+
     return atmF
 
-def calcStructureFactors(HKL, AtomicF, XYZ, form="complete"):
+def npcalcStructureFactors(HKL, AtomicF, XYZ, form="complete"):
     """
     Calculate the complex-valued structure factor (SF)
-    
+
     :Parameters:
         HKL : n x 3 array
             List of n (h,k,l) index triplets
@@ -215,36 +216,77 @@ def calcStructureFactors(HKL, AtomicF, XYZ, form="complete"):
             Real-space fractional coordinates of all atoms (within unit cell)
         form : str
             Numerical form of the structure factor
-    
+
     :Return:
         F : 1D array (n elements)
             Complex-valued structure factor for all index triplets
     """
-    
+
     if form == "cos":
         sinf, cosf = 0, 1
     elif form == "sin":
         sinf, cosf = 1, 0
     elif form == "complete":
         sinf, cosf = 1, 1
+
+    nHKL, _ = HKL.shape
+    HKLdotXYZ = np.dot(HKL, XYZ.T) # n x m array
+    PhaseMatrix = np.cos(2*pi*HKLdotXYZ)*cosf + 1j*np.sin(2*pi*HKLdotXYZ)*sinf # n x m array
+    #SF = np.einsum('ij->i', AtomicF*PhaseMatrix)
+    SF = np.zeros((nHKL,))
+    for ind in nb.prange(nHKL):
+        SF[ind] = np.sum(AtomicF[ind,:]*PhaseMatrix[ind,:])
+
+    return SF
     
-    HKLdotXYZ = HKL.dot(XYZ.T)
-    PhaseMatrix = np.cos(2*pi*HKLdotXYZ)*cosf + 1j*np.sin(2*pi*HKLdotXYZ)*sinf
-    SF = np.einsum('ij->i', AtomicF*PhaseMatrix)
+@nb.njit("complex128[:](float64[:,:], float64[:,:], float64[:,:], int64)")
+def nbcalcStructureFactors(HKL, AtomicF, XYZ, form=2):
+    """
+    Calculate the complex-valued structure factor (SF)
+
+    :Parameters:
+        HKL : n x 3 array
+            List of n (h,k,l) index triplets
+        AtomicF : n x m array
+            Atomic structure factor array (m atoms in a unit cell for the n Miller-indexed triplets)
+        XYZ : m x 3 array
+            Real-space fractional coordinates of all atoms (within unit cell)
+        form : str
+            Numerical form of the structure factor
+
+    :Return:
+        F : 1D array (n elements)
+            Complex-valued structure factor for all index triplets
+    """
     
+    nHKL, _ = HKL.shape
+    HKLdotXYZ = np.dot(HKL, XYZ.T) # n x m array
+    
+    if form == 0: # Cosine
+        PhaseMatrix = np.cos(2*pi*HKLdotXYZ) + 0j
+    elif form == 1: # Sine
+        PhaseMatrix = 1j*np.sin(2*pi*HKLdotXYZ)
+    elif form == 2: # Complete
+        PhaseMatrix = np.cos(2*pi*HKLdotXYZ) + 1j*np.sin(2*pi*HKLdotXYZ)
+    
+    # Calculate the structure factor (SF)
+    SF = np.zeros((nHKL,), dtype=np.complex128)
+    for ind in nb.prange(nHKL):
+        SF[ind] = np.sum(AtomicF[ind,:]*PhaseMatrix[ind,:])
+
     return SF
 
 def calcScatteringVectorsIMG(Detect,eBeam,rotation):
-    
+
     nX = Detect.imgSizeX
     nY = Detect.imgSizeY
     ranX = np.arange(nX, dtype='float64')
     ranY = np.arange(nY, dtype='float64')
-    
+
     qVectorIMG = np.zeros([nY,nX,3], dtype='float64')
     q = np.zeros([nY,nX,3], dtype='float64')
     modQ_IMG = np.zeros([nY,nX], dtype='float64')
-    
+
     X, Y = np.meshgrid(ranX, ranY)
     dx = Detect.pixelSize*(X-Detect.centreX)/Detect.magnification
     dy = Detect.pixelSize*(Y-Detect.centreY)/Detect.magnification
@@ -255,34 +297,34 @@ def calcScatteringVectorsIMG(Detect,eBeam,rotation):
     phi = np.where(dx==0, np.sign(dy)*np.pi/2, np.arctan(dy/dx)+np.pi*(1-np.sign(dx))/2) + rotation
     modK = 1.0/eBeam.wavelength
     modQ_IMG = 2*modK*np.sin(0.5*theta.T)
-    
+
     qR = 2*modQ_IMG*np.cos(0.5*theta) # projection of q on detector
     q[:,:,0] = qR*np.cos(phi) # x-component of q
     q[:,:,1] = qR*np.sin(phi) # y-component of q
-    q[:,:,2] = -2*modQ_IMG*np.sin(0.5*theta) # z-component of q (electron beam is -z direction)    
+    q[:,:,2] = -2*modQ_IMG*np.sin(0.5*theta) # z-component of q (electron beam is -z direction)
     #Transform to reference of crystal
     qVectorIMG = np.einsum('ijk,kl', q, eBeam.axes)
     #qVectorIMG = np.array([[np.dot(q[i,j,:],eBeam.axes) for i in ranY] for j in ranX])
-    
+
     return qVectorIMG, modQ_IMG
 
 def hkl2fAtomicAllAtoms(atomicNumbers, hklArray, axesReciprocal):
     """
     Calculate the atomic structure factor for all (h,k,l)
-    
+
     :Parameters:
         atomicNumbers : 1D array
-            Atomic numbers of the 
+            Atomic numbers of the
         hklArray : ndarray
             Array of (h,k,l) for the diffraction peaks
         axesReciprocal : 3x3 array
             Reciprocal space vectors of the unit cell
-        
+
     :Return:
         fAtomic : ndarray
             Matrix of atomic structure factors
     """
-    
+
     nAtoms = np.size(atomicNumbers)
     atSeq = np.arange(nAtoms)
     nHKL = np.size(hklArray, 0)
@@ -291,20 +333,20 @@ def hkl2fAtomicAllAtoms(atomicNumbers, hklArray, axesReciprocal):
     fAtomic = np.zeros([nHKL, nAtoms])
     qVec = np.dot(hklArray, axesReciprocal) # nx3 2D array
     modQ = np.linalg.norm(qVec, axis=1) # 1D array
-    
+
     for atm in atSeq:
         fAtomic[:, atm] = calcAtomicF(atomicNumbers[atm], modQ)
-    
+
     # print('\nCalculated f_atomic for all ' + str(nAtoms) + ' atoms\n')
     return fAtomic
 
 def calcHKLvaluesRequired(qVecIMG,axesReciprocal):
-    
+
     hklExtremes = np.zeros([4,3])
     row = 0
     for X in [0,1]:
         for Y in [0,1]:
-            A = [0,-1]            
+            A = [0,-1]
             hklExtremes[row,:] = cart2frac(qVecIMG[A[Y],A[X],:],axesReciprocal)
             row = row+1
     print('h,k,l values at corners of detector:\n {}'.format(hklExtremes))
@@ -329,7 +371,7 @@ def calcHKLvaluesRequired(qVecIMG,axesReciprocal):
 def removeUnnecessaryHKLs(HKLvals,eBeam,axesReciprocal,cutoff):
     # eBeam is an instance of the above ElectronBeam class
     # cutoff : thickness of reciprocal space to sample as ratio of |k0|
-    
+
     k0norm = eBeam.dirCart_norm
     nHKL = np.size(HKLvals,0)
     qHKL = np.dot(HKLvals,axesReciprocal)
@@ -339,16 +381,16 @@ def removeUnnecessaryHKLs(HKLvals,eBeam,axesReciprocal,cutoff):
         if distFromPlane < cutoff*eBeam.modK:
             HKLminimal.append(HKLvals[indxH])
     HKL2 = np.array(HKLminimal)
-    
+
     return HKL2
 
 def calcFhkl(hkl, atomCoords, atomicNos, axesReciprocal):
-    
+
     # Calculate structure factors
     fAtomic_HKL = hkl2fAtomicAllAtoms(atomicNos, hkl, axesReciprocal)
     fractionals = cart2frac(atomCoords, axesReciprocal)
     Fhkl = calcStructureFactors(hkl, fAtomic_HKL, fractionals)
-    
+
     return Fhkl
 
 
@@ -364,99 +406,99 @@ def calcFhkl(hkl, atomCoords, atomicNos, axesReciprocal):
 def lincompose(lincoeffs, components, mat_shape=None):
     """ Linear composition of bases
     """
-    
+
     linmodel = np.dot(lincoeffs, components)
-    
+
     try:
         matrecon = linmodel.reshape(mat_shape)
     except:
         matrecon = linmodel
-    
+
     return matrecon
-    
+
 def polycompose(lincoeffs, components, axis=0, order=2, mat_shape=None):
     """ Polynomial composition of linear bases
     """
-    
+
     nlincoeffs = kron2d(lincoeffs, axis=axis, order=order)
     nlincomps = kron2d(components, axis=axis, order=order)
     polymodel = np.dot(nlincoeffs, nlincomps)
-    
+
     try:
         matrecon = polymodel.reshape(mat_shape)
     except:
         matrecon = polymodel
-    
+
     return matrecon
-    
+
 def kron2d(mat, axis=0, order=2):
     """ Calculate the multinomial feature vectors
     """
-    
+
     if np.ndim(mat) == 1:
         mat = mat[:, np.newaxis]
     mat = np.rollaxis(mat, axis)
     nvec, nfeatlen = mat.shape
-    
+
     # List the cross terms
     crosst = list(it.combinations(range(nvec), 2))
     # List the diagonal terms
     cardt = [(i, i) for i in range(nvec)]
-    
+
     nterms = u.cnr(order+nvec-1, nvec-1)
     matkron = np.zeros((nterms, nfeatlen))
-    
+
     # Calculate the diagonal terms
     matkron[:nvec, :] = mat**2
     # Calculate the cross terms
     for i, term in enumerate(crosst):
         matkron[nvec+i, :] = 2*mat[term[0], :]*mat[term[1], :]
-    
+
     matkron = np.rollaxis(matkron, axis)
-    
+
     return matkron.squeeze()
-    
+
 # ============== Loss functions ============== #
 
 def linear_unmix_loss(coeffs, Sexp, Scomps, A):
     """
     Loss function for linear unmixing
     """
-    
+
     # coeffs -- coefficients
     # Sexp -- vectorized experimental difference map
     # Scomps -- composite matrix made of unmixing bases
-    
+
     Smodel = np.dot(A, Scomps)
     loss = np.sum(Sexp.ravel() - Smodel)**2
-    
+
     return loss
-    
+
 def nonlinear_unmix_loss(coeffs, Sexp, Scomps, A, order=2):
     """
     Loss function for nonlinear unmixing
     """
-    
+
     linloss = linear_unmix_loss(coeffs, Sexp, Scomps, A)
     polydiff = (Sexp.ravel())**order - polycompose(coeffs, Scomps)
     polyloss = np.sum(polydiff**2)
-    
+
     loss = linloss + polyloss
-    
+
     return loss
-    
+
 def simloss(a, diffdata, dispmat, xyzgs, imgoff, mask, atoms, bd):
     """
     Loss function for simulated-based optimization
     """
-    
+
     totaldisp = np.dot(dispmat, a).reshape(xyzgs.shape)
     on_structure = xyzgs + totaldisp
-    
+
     imgon = ps.simulate2(atoms, xyzgs, on_structure, mag=1.11, rot=137, beamsize=300e-6, exrat=0.2, \
                          mosL=2.0, mosR=1.0, beam_direction=-bd, method='rotmosaic', rebin=(2,2))
     diffsim = 7e-6*(imgon - imgoff)*mask
-    
+
     loss = np.linalg.norm(diffdata - diffsim)**2
-    
+
     return loss
